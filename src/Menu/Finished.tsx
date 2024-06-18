01c3a478f4b688d0d2553e1e3d0d6d42630c21d9
davinrdh <davinridhas@gmail.com>
2024-06-18 13:55:52 +0700

    update score

--- src/Menu/Finished.tsx
+++ src/Menu/Finished.tsx
@@ -7,8 +7,9 @@ export default function Finished({ correctAnswer }: any) {
     <div>
       <h1 className="text-center text-danger fw-bold">Finished!</h1>
       {correctAnswer?.length === 0 && <h1 className="text-center">You got {correctAnswer.length} correct answers! Get to know members better!</h1>}
-      {correctAnswer?.length >= 1 && correctAnswer?.length <= 5 && <h1 className="text-center">Congrats, You got {correctAnswer.length} correct answers! You are a wota smart</h1>}
-      {correctAnswer?.length > 5 && correctAnswer?.length < 10 && <h1 className="text-center">Congrats, You got {correctAnswer.length} correct answers! You are a wota genius</h1>}
+      {correctAnswer?.length >= 1 && correctAnswer?.length <= 3 && <h1 className="text-center">Congrats, You got {correctAnswer.length} correct answers! You are a wota</h1>}
+      {correctAnswer?.length >= 4 && correctAnswer?.length <= 6 && <h1 className="text-center">Congrats, You got {correctAnswer.length} correct answers! You are a wota smart</h1>}
+      {correctAnswer?.length >= 7 && correctAnswer?.length < 10 && <h1 className="text-center">Congrats, You got {correctAnswer.length} correct answers! You are a wota genius</h1>}
       {correctAnswer?.length === 10 && <h1 className="text-center">Congrats, Your answers are perfect! You are a wota sevvuh</h1>}
       <p className="">Do you want to play again? <NavLink to={'/'}>Click here!</NavLink></p>
     </div>
